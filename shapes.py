c082cf0b7c1ee4f4082a42786b9d12f72669401c
Ruchitha Chinta <122854121+ruchitha36@users.noreply.github.com>
2023-02-07 11:51:56 +0530

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,27 @@
+# Print the Following Pattern
+#    +----------+
+
+print("+----------+")
+# the above is the one way
+
+# The another way is
+
+print("+"+ 10 * "-"+ "+")
+
+# print the following pattern
+#    *----------*
+
+print("*"+ 10 * "-"+ "*")
+
+# print the following pattern
+#    +----------+
+#    |          |
+#    |          |
+#    |          |
+#    |          |
+#    |          |
+#    +----------+
+
+print("+"+10*"-"+"+")
+print(("|"+10*" "+"|\n")*5,end="")
+print("+"+10*"-"+"+")
